a45ee32319032ea6f1be1d0190070c032fccccee
luis <foreverluiscastro@gmail.com>
2021-03-22 14:50:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(arr, string) {
+    return arr.filter(argument1 => argument1.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(arr, string) {
+    return arr.filter(argument2 => argument2[0, 1] === string[0, 1]);
+}
+
+function matchName(Object, string) {
+    return Object.filter(argument3 => argument3.name === string);
+}
